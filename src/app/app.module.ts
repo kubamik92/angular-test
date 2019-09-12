36a2d2305bbd9a48c281eb667ede1e2ff885e10e
Kuba <jakubm@imag.pl>
2019-09-12 14:03:23 +0200

    added nav-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { RouterModule } from '@angular/router';
+import { OMnieComponent } from './o-mnie/o-mnie.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    OMnieComponent,
+    KontaktComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      
+      { path: 'o-mnie', component: OMnieComponent },
+      { path: 'kontakt', component: KontaktComponent },
+    
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/o-mnie/o-mnie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-o-mnie',
+  templateUrl: './o-mnie.component.html',
+  styleUrls: ['./o-mnie.component.css']
+})
+export class OMnieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
